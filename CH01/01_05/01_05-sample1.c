b4ce83e339bf4422fbd1b0efa1639a7c4ec0105c
Jack C <hshstheninja@Proton.me>
2024-12-09 10:55:09 +1030

    sync

--- CH01/01_05/01_05-sample1.c
+++ CH01/01_05/01_05-sample1.c
@@ -3,16 +3,16 @@
 
 int main()
 {
-	int a,b;
+	int a, b;
 
 	printf("Type a positive value: ");
-	scanf("%d",&b);
-	for(a=0;a<b;a++)
+	scanf("%d", &b);
+	for (a = 0; a < b; a++)
 	{
-		printf("I must do this %d times\n",b);
-		if(a==9)
+		printf("I must do this %d times\n", b);
+		if (a == 9)
 			break;
 	}
-	
+
 	return 0;
 }
